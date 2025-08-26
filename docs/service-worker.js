86427d1b8f38210e38189aa10ba7e00351ffe197
jramirez5-alt <jramirez5@ucol.mx>
2025-08-26 09:04:52 -0600

    Agregando archivos nuevos

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -6,9 +6,9 @@ const URLS_TO_CACHE = [
   "./icon-192.png",
   "./icon-512.png"
 ];
-
-holla 
+ 
 self.addEventListener("install", event => {
+  
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
